0cb5dc3dd505db37fd45d1d853a27c55199e5e86
Jônatan de Castro Santos Paiva <jonatan.paiva@sipam.gov.br>
2024-03-04 15:21:21 -0300

    feat: game-movie api connection logic

--- guessr/src/app/components/game/movie/game-movie-entry/game-movie-entry.component.ts
+++ guessr/src/app/components/game/movie/game-movie-entry/game-movie-entry.component.ts
@@ -1,12 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieAPIService } from '../../../../core/services/movie-api.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-game-movie-entry',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, FormsModule ],
   templateUrl: './game-movie-entry.component.html',
-  styleUrl: './game-movie-entry.component.css'
+  styleUrls: ['./game-movie-entry.component.css']
 })
-export class GameMovieEntryComponent {
+
+export class GameMovieEntryComponent implements OnInit {
+  movieSuggestions: any[] = [];
+  selectedMovie: any;
+  selectedMovieTitle: string = '';
+  showSuggestions: boolean = false;
+
+  constructor( private movieAPIService: MovieAPIService) {}
+
+  ngOnInit(): void {
+  }
+
+  async searchMovies(event: any) {
+    const query = event.target.value;
+    if (query) {
+      try {
+        this.movieSuggestions = await this.movieAPIService.fetchSuggestions(query);
+        this.showSuggestions = true; 
+      } catch (error) {
+        console.error('Erro ao buscar sugestões de filmes:', error);
+      }
+    } else {
+      this.movieSuggestions = []; // Limpa a lista de sugestões se a consulta estiver vazia
+      this.showSuggestions = false;
+    }
+  }
+
+  selectMovie(movie: any) {
+    this.selectedMovie = movie;
+    this.selectedMovieTitle = movie.title; // Atualiza o título do filme selecionado
+    this.movieSuggestions = []; // Limpar a lista de sugestões após selecionar o filme
+  }
+
+  submitMovie(event: any) {
+    if (this.selectedMovie) {
+      const movieId = this.selectedMovie.id; // Obtém a ID do filme selecionado
+      console.log('ID do filme selecionado:', movieId);
+      
+      // Chamar a função para buscar informações do filme pelo ID
+    this.movieAPIService.getMovieDetails(movieId)
+    .then((movieDetails: any) => {
+      console.log('Detalhes do filme:');
+      console.log(movieDetails.title);
+      console.log(movieDetails.release_date);
+      console.log(movieDetails.poster_path);
+    })
+
+    this.movieAPIService.getMovieCredits(movieId)
+    .then((movieCredits: any) => {
+      //const director = movieCredits.crew.find((member: { name: string, department: string }) => member.department === "Directing");
+      //console.log("Nome do diretor:", director.name);
+      const directors = movieCredits.crew.filter((member: { name: string, department: string, job: string }) => member.department === "Directing" && member.job === "Director");
+      console.log("Direção:");
+      directors.forEach((director: { name: any; }) => {
+        console.log(director.name);
+      });
+    })
+    .catch((error: any) => {
+      console.error('Erro ao buscar detalhes do filme:', error);
+    });
+    }
+  }
+
 
 }
---
+++ guessr/src/app/core/services/movie-api.service.ts
@@ -0,0 +1,100 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieAPIService {
+  private access_token?: string;
+
+  constructor() {
+    this.access_token = '';
+  }
+
+
+  async getToken(): Promise<string> {
+    try {
+      const credentials = 'CredentialKey';
+      const response = await fetch('https://api.themoviedb.org/3/authentication', {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer ' + credentials,
+        },
+      });
+
+      const data = await response.json();
+      return credentials;
+    } catch (error) {
+      console.error('Ocorreu um erro de autorização da API: ', error);
+      throw new Error('Erro de autenticação com API.');
+    }
+  }
+
+  async fetchSuggestions(query: string, language = 'pt-br'): Promise<any[]> {
+    try {
+      const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&language=${language}`, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer ' + this.access_token,
+        },
+      });
+      const data = await response.json();
+      return data.results;
+    } catch (error) {
+      console.error('Ocorreu um erro ao retornar sugestões: ', error);
+      throw new Error('Falha ao buscar sugestões.');
+    }
+  }
+
+  async getMovieImage(movie_id: number): Promise<any> {
+    try {
+      const response = await fetch(`https://api.themoviedb.org/3/movie/${movie_id}/images`, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer ' + this.access_token,
+        },
+      });
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Ocorreu um erro ao recuperar valores de imagens: ', error);
+      throw new Error('Erro ao recuperar imagens da API.');
+    }
+  }
+
+  async getMovieDetails(movie_id: number, language = 'pt-br'): Promise<any> {
+    try {
+      const response = await fetch(`https://api.themoviedb.org/3/movie/${movie_id}?language=${language}`, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer ' + this.access_token,
+        },
+      });
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Ocorreu um erro ao recuperar informações do filme: ', error);
+      throw new Error('Erro ao recuperar informações do filme.');
+    }
+  }
+
+  async getMovieCredits(movie_id: number, language = 'pt-br'): Promise<any> {
+    try {
+      const response = await fetch(`https://api.themoviedb.org/3/movie/${movie_id}/credits?language=${language}`, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer ' + this.access_token,
+        },
+      });
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Ocorreu um erro ao recuperar informações do filme: ', error);
+      throw new Error('Erro ao recuperar informações do filme.');
+    }
+  }
+}
