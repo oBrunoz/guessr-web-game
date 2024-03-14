e2713778209342c67e91400de1b7e87324c6b19c
Bruno de Alencar Ferreira <bruno.ferreira@sipam.gov.br>
2024-03-14 16:50:40 -0300

    chore: get manda imgs component

---
+++ guessr/src/app/components/game/manga/game-manga-img/game-manga-img.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-game-manga-img',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './game-manga-img.component.html',
+  styleUrl: './game-manga-img.component.css'
+})
+export class GameMangaImgComponent {
+  @Input() imageUrl: string = '';
+
+  constructor() {console.log(this.imageUrl)}
+}
